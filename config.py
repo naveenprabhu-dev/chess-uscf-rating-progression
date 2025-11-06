b4ff7fd9f53750f56bb5ad3346464f5ef3d9155e
naveenprabhu-dev <112416952+naveenprabhu-dev@users.noreply.github.com>
2025-11-06 11:00:53 -0500

    Clarify RATING_MILESTONES with a comment
    Added comment to clarify the purpose of RATING_MILESTONES.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-RATING_MILESTONES = [400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200]
+RATING_MILESTONES = [400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200] # Edit to your needs 
 ACCESS_FILE = " " # Input access file name here
 SHEET_NAME = " " # Input sheet name here
 
